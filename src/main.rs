4d03e68f69885e4a3c55ed2712f7c03fba37e4b0
Fredrik <freddahf@outlook.com>
2023-08-26 18:24:47 +0200

    adding better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 mod weather;
 
+use core::fmt;
+use std::{
+    error::Error,
+    fmt::{Display, Formatter},
+};
+
 use axum::{
     extract::{Query, State},
+    response::{IntoResponse, Response},
     routing::get,
     Json, Router, Server,
 };
@@ -10,19 +17,55 @@ use reqwest::Client;
 use serde_json::Value;
 use weather::{Alert, Location, MetAlert, MetNowcast, Nowcast, OpenWeatherNowcast};
 
-async fn alerts(State(client): State<Client>) -> Result<Json<Vec<Alert>>, StatusCode> {
+#[derive(Debug)]
+pub struct AppError {
+    message: String,
+}
+
+impl Display for AppError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl Error for AppError {}
+
+impl AppError {
+    pub fn new(message: impl Into<String>) -> Self {
+        Self {
+            message: message.into(),
+        }
+    }
+
+    pub fn message(&self) -> &str {
+        &self.message
+    }
+}
+
+// Tell axum how to convert `AppError` into a response.
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            self.message().to_string(),
+        )
+            .into_response()
+    }
+}
+
+async fn alerts(State(client): State<Client>) -> Result<Json<Vec<Alert>>, AppError> {
     let res = client
         .get("https://api.met.no/weatherapi/metalerts/1.1/.json")
         .send()
         .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+        .map_err(|_| AppError::new("request failed"))?;
     let json = res
         .json::<Value>()
         .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+        .map_err(|_| AppError::new("Deserialization failed"))?;
     let alerts: Vec<Alert> = json["features"]
         .as_array()
-        .ok_or(StatusCode::INTERNAL_SERVER_ERROR)?
+        .ok_or(AppError::new("Failed to convert value to alert type"))?
         .iter()
         .filter_map(|alert| MetAlert::try_from(alert.clone()).ok())
         .map(|alert| alert.into())
@@ -33,30 +76,30 @@ async fn alerts(State(client): State<Client>) -> Result<Json<Vec<Alert>>, Status
 async fn nowcasts(
     State(client): State<Client>,
     Query(location): Query<Location>,
-) -> Result<Json<Vec<Nowcast>>, StatusCode> {
+) -> Result<Json<Vec<Nowcast>>, AppError> {
     let met_cast: MetNowcast = client
         .get("https://api.met.no/weatherapi/nowcast/2.0/complete")
         .query(&[("lat", location.lat), ("lon", location.lon)])
         .send()
         .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .map_err(|_| AppError::new("request failed"))?
         .json::<Value>()
         .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .map_err(|_| AppError::new("Deserialization failed"))?
         .try_into()
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+        .map_err(|_| AppError::new("Failed to convert value to nowcast type"))?;
     let openweathermap: OpenWeatherNowcast = client
         .get("https://api.openweathermap.org/data/2.5/weather")
         .query(&[("lat", location.lat), ("lon", location.lon)])
         .query(&[("appid", env!("OPENWEATHERMAPAPIKEY"))])
         .send()
         .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .map_err(|_| AppError::new("request failed"))?
         .json::<Value>()
         .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .map_err(|_| AppError::new("Deserialization failed"))?
         .try_into()
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+        .map_err(|_| AppError::new("Failed to convert value to nowcast type"))?;
     let nowcasts: Vec<Nowcast> = vec![met_cast.into(), openweathermap.into()];
     Ok(Json(nowcasts))
 }
