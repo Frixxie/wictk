0114c1c67269fd67be514ad8ac40210abef11daf
Fredrik <freddahf@outlook.com>
2023-08-26 19:53:08 +0200

    group the api

--- src/main.rs
+++ src/main.rs
@@ -116,10 +116,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     );
     let client = client_builder.user_agent(APP_USER_AGENT).build().unwrap();
 
-    let app = Router::new()
+    let api = Router::new()
         .route("/alerts", get(alerts))
         .route("/nowcasts", get(nowcasts))
         .with_state(client);
+
+    let app = Router::new().nest("/api", api);
 
     Server::bind(&"0.0.0.0:3000".parse()?)
         .serve(app.into_make_service())
